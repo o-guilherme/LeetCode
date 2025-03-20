c2049e1f3cb64cadb424beb736e1687358e47586
Guilherme <13070898+o-guilherme@users.noreply.github.com>
2025-03-19 23:10:20 -0300

    SQL Exercício 1

---
+++ SQL 50/1.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
+
+
+/*  PRIMEIRO COMANDO
+# Write your MySQL query statement below
+SELECT *
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
+
+Erros: Não especifiquei a coluna correta.
+*/
+ 
+-- Runtime: 132 ms
+-- Nada a otimizar usando um comando só.
+
